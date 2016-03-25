d6b24e93452ba274d33aec0575173e5563c19db0
Stephen Tridgell <stephen@tridgell.net>
2016-03-25 11:02:20 +1100

    Add some comments to explain things

--- src/main/scala/top.scala
+++ src/main/scala/top.scala
@@ -1,6 +1,8 @@
 import Chisel._
 import chiseltemplate._
 
+/** This file is used to generate verilog but can be used for anything you want
+  */
 object top {
   def main(args: Array[String]): Unit = {
     val chiselArgs = args.drop(1)
--- src/test/scala/MyTopLevelModuleSuite.scala
+++ src/test/scala/MyTopLevelModuleSuite.scala
@@ -1,15 +1,24 @@
+// These imports are needed for scala test
 import org.junit.Assert._
 import org.junit.Test
 import org.junit.Ignore
+
+// These are other scala imports
 import scala.util.Random
 import scala.collection.mutable.ArrayBuffer
 
+// Chisel imports
 import Chisel._
 import chiseltemplate.MyTopLevelModule
 
+/** TestSuite should test only one module
+  * It should be named [MyModuleName]Suite
+  */
 class MyTopLevelModuleSuite extends TestSuite {
 
+  // You can have multiple tests in a test suite
   @Test def topModTest {
+    // Put test code here ...
     class MyTopLevelModuleTests( c : MyTopLevelModule ) extends Tester(c) {
       val cycles = 30
       val myRand = new Random
